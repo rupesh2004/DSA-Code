8864a99dc3a55d6212d15d14736040f86f5b6499
rupesh2004 <bhosalerupesh67@gmail.com>
2024-03-06 08:51:05 +0530

    06-03-2024

---
+++ lec2/typecasting.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a='a';
+    cout<<"\n A : "<<a;
+
+    char b=97;
+    cout<<"\n B : "<<b;
+
+    int c=-5;
+    cout<<"\n C : "<<c;
+
+    unsigned int d=-1234;
+    cout<<"\n D : "<<d;
+
+    bool e=a==b;
+    cout<<"\n E : "<<e;
+}
